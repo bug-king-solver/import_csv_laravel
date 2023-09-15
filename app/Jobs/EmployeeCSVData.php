2cbf3a2e42a5be20b028ced0c85caadda54499e7
jannydream0826 <janny.dream.high@gmail.com>
2023-09-16 02:24:27 +0900

    feat: define job for employeecsvdata

---
+++ app/Jobs/EmployeeCSVData.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Imports\EmployeeImport;
+use Maatwebsite\Excel\Facades\Excel;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Bus\Batchable;
+
+class EmployeeCSVData implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+    
+    protected $filePath;
+
+    /**
+     * Create a new job instance.
+     */
+    public function __construct($filePath)
+    {
+        $this->filePath = $filePath;
+    }
+
+    /**
+     * Execute the job.
+     */
+    public function handle(): void
+    {
+        try {
+            // Import the CSV using your custom import class
+            Excel::import(new EmployeeImport, $this->filePath);
+        } catch (\Exception $e) {
+            // Log any exceptions for debugging
+            Log::error('Error importing CSV: ' . $e->getMessage());
+        }
+    }
+}
