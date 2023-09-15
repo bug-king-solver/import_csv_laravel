3a28464e5580dd927ec5e155d25de1bb1bbe14ea
jannydream0826 <janny.dream.high@gmail.com>
2023-09-16 02:20:29 +0900

    feat: define migrations for employees and jobs

---
+++ database/migrations/2023_09_15_061818_create_employees_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('employees', function (Blueprint $table) {
+            $table->id();
+            $table->string('employee_id')->unique();
+            $table->string('name_prefix');
+            $table->string('first_name');
+            $table->string('middle_initial');
+            $table->string('last_name');
+            $table->string('gender');
+            $table->string('email');
+            $table->date('date_of_birth');
+            $table->time('time_of_birth');
+            $table->float('age_in_years');
+            $table->date('date_of_joining');
+            $table->float('age_in_company_years');
+            $table->string('phone_no');
+            $table->string('place_name');
+            $table->string('county');
+            $table->string('city');
+            $table->string('zip');
+            $table->string('region');
+            $table->string('user_name');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('employees');
+    }
+};
