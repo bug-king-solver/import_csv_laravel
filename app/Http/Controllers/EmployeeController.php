cdf8652a751e73a08b25105b619177677574aa3b
jannydream0826 <janny.dream.high@gmail.com>
2023-09-17 12:30:25 +0900

    feat: define controller functions

--- app/Http/Controllers/EmployeeController.php
+++ app/Http/Controllers/EmployeeController.php
@@ -7,9 +7,36 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Facades\DB;
+use App\Models\Employee;
 
 class EmployeeController extends Controller
 {
+
+    public function index()
+    {
+        $employees = Employee::all();
+        return response()->json($employees);
+    }
+
+    public function show($id)
+    {
+        $employee = Employee::find($id);
+        if (!$employee) {
+            return response()->json(['message' => 'Employee not found'], 404);
+        }
+        return response()->json($employee);
+    }
+
+    public function destroy($id)
+    {
+        $employee = Employee::find($id);
+        if (!$employee) {
+            return response()->json(['message' => 'Employee not found'], 404);
+        }
+        $employee->delete();
+        return response()->json(['message' => 'Employee deleted'], 200);
+    }
+
     /**
      * Write code on Method
      *
