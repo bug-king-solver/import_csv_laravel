dcb3b7491b98f15d66a1d434559049ab89d46b21
jannydream0826 <janny.dream.high@gmail.com>
2023-09-19 01:32:50 +0900

    fix: update styles

--- app/Jobs/EmployeeCSVData.php
+++ app/Jobs/EmployeeCSVData.php
@@ -5,12 +5,10 @@ namespace App\Jobs;
 use App\Imports\EmployeeImport;
 use Maatwebsite\Excel\Facades\Excel;
 use Illuminate\Bus\Queueable;
-use Illuminate\Contracts\Queue\ShouldBeUnique;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
-use Illuminate\Bus\Batchable;
 
 class EmployeeCSVData implements ShouldQueue
 {
